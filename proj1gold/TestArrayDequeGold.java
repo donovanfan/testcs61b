48d0ce0dec433f46553b51b30fc5f6d48c8be02a
donovanfan <donovanfan97@gmail.com>
2020-07-26 17:32:03 -0700

    proj1 gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,50 +1,69 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestArrayDequeGold {
-    private static String message = "";
+    import org.junit.Test;
+import static org.junit.Assert.*;
 
-    private void modifiedAdd(double rand, Integer i, StudentArrayDeque
-            <Integer> ss, ArrayDequeSolution<Integer> cs) {
-        if (rand >= 0.5) {
-            ss.addFirst(i);
-            cs.addFirst(i);
-            this.message += String.format("\naddFirst(%d)", i);
-            return;
-        }
-        ss.addLast(i);
-        cs.addLast(i);
-        this.message += String.format("\naddLast(%d)", i);
-    }
+    public class TestArrayDequeGold {
 
-    private void modifiedRemove(double rand, StudentArrayDeque
-            <Integer> ss, ArrayDequeSolution<Integer> cs) {
-        Integer expected, actual;
-        if (rand >= 0.5) {
-            actual = ss.removeFirst();
-            expected = cs.removeFirst();
-            this.message += "\nremoveFirst()";
-        } else {
-            actual = ss.removeFirst();
-            expected = cs.removeFirst();
-            this.message += "\nremoveLast()";
-        }
-        assertEquals(message, expected, actual);
-    }
+        private static final int nCall = 1000; // How many to call methods randomly
+        private static String message = ""; // Store failure sequence
 
-    @Test
-    public void tester() {
-        StudentArrayDeque<Integer> ss = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> cs = new ArrayDequeSolution<>();
-        for (int i = 0; i < 100; i++) {
-            double rand1 = StdRandom.uniform(1);
-            double rand2 = StdRandom.uniform(1);
-            if (ss.size() == 0) {
-                modifiedAdd(rand2, StdRandom.poisson(10), ss, cs);
-            } else if (rand1 >= 0.5) {
-                modifiedAdd(rand2, StdRandom.poisson(10), ss, cs);
+        /** Given uniformly distributed random double between 0 and 1,
+         * randomly adds Integer to deque.
+         * */
+        private void randomAdd(double random, Integer i, StudentArrayDeque<Integer> sad, ArrayDequeSolution<Integer> ads) {
+            if (random < 0.5) {
+                sad.addFirst(i);
+                ads.addFirst(i);
+                message += "\naddFirst(" + i + ")";
             } else {
-                modifiedRemove(rand2, ss, cs);
+                sad.addLast(i);
+                ads.addLast(i);
+                message += "\naddLast(" + i + ")";
             }
         }
+
+        /** Given uniformly distributed random double between 0 and 1,
+         * randomly adds Integer to deque.
+         * */
+        private void randomRemove(double random, Integer i, StudentArrayDeque<Integer> sad, ArrayDequeSolution<Integer> ads) {
+            Integer expected;
+            Integer actual;
+            if (random < 0.5) {
+                expected = ads.removeFirst();
+                actual = sad.removeFirst();
+                message += "\nremoveFirst()";
+            } else {
+                expected = ads.removeLast();
+                actual = sad.removeLast();
+                message += "\nremoveLast()";
+            }
+            assertEquals(message, expected, actual);
+        }
+
+        @Test
+        public void testRandomized() {
+            StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+            ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+
+            for (Integer i = 0; i < nCall; i += 1) {
+                if (sad.isEmpty()) {
+                    double random = StdRandom.uniform();
+                    randomAdd(random, i, sad, ads);
+                } else {
+                    double random1 = StdRandom.uniform();
+                    double random2 = StdRandom.uniform();
+                    if (random1 < 0.5) {
+                        randomAdd(random2, i, sad, ads);
+                    } else {
+                        randomRemove(random2, i, sad, ads);
+                    }
+                }
+            }
+
+        }
+
     }
 }
