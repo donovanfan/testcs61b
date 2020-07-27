dafc51ac1171ae637766d7dc957a83484caf5d5b
donovanfan <donovanfan97@gmail.com>
2020-07-26 17:20:48 -0700

    proj1 gold

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class TestArrayDequeGold {
+    private static String message = "";
+
+    private void modifiedAdd(double rand, Integer i, StudentArrayDeque
+            <Integer> ss, ArrayDequeSolution<Integer> cs) {
+        if (rand >= 0.5) {
+            ss.addFirst(i);
+            cs.addFirst(i);
+            this.message += String.format("addFirst(%d)\n", i);
+            return;
+        }
+        ss.addLast(i);
+        cs.addLast(i);
+        this.message += String.format("addLast(%d)\n", i);
+    }
+
+    private void modifiedRemove(double rand, StudentArrayDeque
+            <Integer> ss, ArrayDequeSolution<Integer> cs) {
+        Integer expected, actual;
+        if (rand >= 0.5) {
+            actual = ss.removeFirst();
+            expected = cs.removeFirst();
+            this.message += "removeFirst()\n";
+        } else {
+            actual = ss.removeFirst();
+            expected = cs.removeFirst();
+            this.message += "removeLast()\n";
+        }
+        assertEquals(message, expected, actual);
+    }
+
+    @Test
+    public void tester() {
+        StudentArrayDeque<Integer> ss = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> cs = new ArrayDequeSolution<>();
+        for (int i = 0; i < 100; i++) {
+            double rand1 = StdRandom.uniform(1);
+            double rand2 = StdRandom.uniform(1);
+            if (ss.size() == 0) {
+                modifiedAdd(rand2, StdRandom.poisson(10), ss, cs);
+            } else if (rand1 >= 0.5) {
+                modifiedAdd(rand2, StdRandom.poisson(10), ss, cs);
+            } else {
+                modifiedRemove(rand2, ss, cs);
+            }
+        }
+    }
+}
