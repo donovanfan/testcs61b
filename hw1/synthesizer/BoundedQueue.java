e7a8fc70db506db83e4a499e8375977155721cd9
donovanfan <donovanfan97@gmail.com>
2020-07-29 19:59:23 -0700

    refresh

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,22 @@
+package synthesizer;
+
+public interface BoundedQueue<T> {
+    /** Return size of the buffer. */
+    int capacity();
+
+    /** Return number of items currently in the buffer. */
+    int fillCount();
+
+    /** Add item x to the end. */
+    void enqueue(T x);
+
+    /** Delete and return item from the front. */
+    T dequeue();
+
+    /** Return (but do not delete) item from the front. */
+    T peek();
+
+    default boolean isEmpty() {
+        return (fillCount() == 0);
+    }
+}
